fef8f1f98c2d13529546b106a04ff532484686fb
holzmano <lenz.baumann2@uzh.ch>
2021-10-13 11:17:08 +0200

    Fixing status bar error, that didnt update the status icons properly

--- uploader/static/uploader/js/refresh_list.js
+++ uploader/static/uploader/js/refresh_list.js
@@ -2,8 +2,11 @@ $(document).ready(function () {
     $.ajaxSetup({cache: false}); // This part addresses an IE bug.  without it, IE will only load the first number and will never refresh
     var my_refresh = setInterval(function () {
         $('td[id^="status_"]').each(function () {
-            $(this).load(" #"+$(this).attr("id"))
+            var name = "#" + $(this).attr("id")
+            $.get('', function (data) {
+                var new_status = $(data).find(name);
+                $(name).replaceWith(new_status);
+            });
         });
     }, 3000);
 });
-
