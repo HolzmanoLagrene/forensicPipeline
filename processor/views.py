9e51891a888b27968c47838a5179236c1710093d
holzmano <lenz.baumann2@uzh.ch>
2021-10-15 20:04:54 +0200

    Allow processing all items

--- processor/views.py
+++ processor/views.py
@@ -1,4 +1,4 @@
-from celery import chain
+from celery import chain, group
 from django.shortcuts import redirect
 
 from processor.tasks import *
@@ -18,4 +18,16 @@ def analyze_one(request, id):
 
 
 def analyze_all(request):
+    identifiers = [identifier.hashsum for identifier in UploadData.objects.all()]
+    chains = []
+    for identifier in identifiers:
+        chains.append(chain(
+        run_log2timeline_background.si(identifier).on_error(log_error.s()),
+        run_pinfo_background.si(identifier).on_error(log_error.s()),
+        run_psort_background.si(identifier).on_error(log_error.s())
+    ))
+    all_workflows = group(
+        *chains
+    )
+    all_workflows.delay()
     return redirect("/")
